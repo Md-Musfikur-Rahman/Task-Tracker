7ccd9ef29d846b57404f73b69c2373e250b4cfc2
MMR Masum <masum@gmail.com>
2022-09-04 10:10:11 +0600

    admin home page 50%

--- admin_home.php
+++ admin_home.php
@@ -18,7 +18,78 @@ include_once './head.php';
         <h1>Home</h1>
 
         <main>
+            <div class="card">
+                <ul>
+                    <li>
+                        <h2>User Counts</h2>
+                        <div class="user">
+                            <div class="users">
+                                <i class="fas fa-user-shield"></i>
+                                <h1><?php
+                                include_once './dbconnect.php';
 
+                                $sql = "SELECT COUNT(name) AS Users FROM user WHERE name!='admin'";
+                                $res = $conn->query($sql);
+                                
+                                $row = $res->fetch_assoc();
+                                echo $row["Users"];
+                                ?> </h1>
+                                <h4>Active Users</h4>
+                            </div>
+                            <div class="admins">
+                                <i class="fas fa-user-lock"></i>
+                                <h1><?php
+                                include_once './dbconnect.php';
+
+                                $sql = "SELECT COUNT(name) AS Users FROM user WHERE name='admin'";
+                                $res = $conn->query($sql);
+                                
+                                $row = $res->fetch_assoc();
+                                echo $row["Users"];
+                                ?> </h1>
+                                <h4>Admins</h4>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <h2>Total Tasks Added</h2>
+                        <div class="det">
+                            <i class="fas fa-tasks-alt"></i>
+                            <h1>
+                                <?php
+                                    include_once './dbconnect.php';
+                            
+                                    $sql = "SELECT COUNT(taskname) AS Users FROM task";
+                                    $res = $conn->query($sql);
+                                    
+                                    $row = $res->fetch_assoc();
+                                    echo $row["Users"];
+                                ?>
+                            </h1>
+                            <h4>Tasks form all users</h4>
+                        </div>
+                    </li>
+                    <li>
+                        <h2>Contacts counts</h2>
+                        <div class="det">
+                            <i class="fas fa-address-card"></i>
+                            <h1>
+                                <?php
+                                 include_once './dbconnect.php';
+                        
+                                 $sql = "SELECT COUNT(fname) AS Users FROM contacts";
+                                 $res = $conn->query($sql);
+                                 
+                                 $row = $res->fetch_assoc();
+                                 echo $row["Users"];
+                             ?>
+                            </h1>
+                            <h4>Contact form all users</h4>
+                        </div>
+
+                    </li>
+                </ul>
+            </div>
         </main>
 
 
