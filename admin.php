ae2e64c91badd8efe5cf1cc76e27fea06e849e6e
MMR Masum <masum@gmail.com>
2022-09-04 09:11:04 +0600

    admin profile page done

--- admin.php
+++ admin.php
@@ -24,10 +24,50 @@ include_once './head.php';
         echo $email;
 
         ?>
+
         </h2>
 
         <main>
-
+            <div class="wrapper">
+                <div class="profile">
+                    <div class="overlay">
+                        <div class="about">
+                            <?php
+                        include './dbconnect.php';
+                        
+                        $email = $_SESSION["email"];                                                                
+                          $sql = "SELECT * FROM  user WHERE email = '$email'";
+                          $result = $conn->query($sql);
+                
+                          if ($result->num_rows > 0) {
+                            // output data of each row
+                            while($row = $result->fetch_assoc()) {
+                            echo '<h4>'.$row["name"].'</h4>';
+                            }
+                        } else {
+                            echo "0 results";
+                        }
+                
+                    ?>
+                            <span>Web Developer</span>
+                        </div>
+                        <ul class="social-icons">
+                            <li><a href="https://www.facebook.com/mmrmasum99/" target="_blank">
+                                    <i class="fa fa-facebook"></i>
+                                </a></li>
+                            <li><a href="https://www.github.com/Md-Musfikur-Rahman/" target="_blank">
+                                    <i class="fa fa-github"></i>
+                                </a></li>
+                            <li><a href="https://www.twitter.com/Mmrmasum5" target="_blank">
+                                    <i class="fa fa-twitter"></i>
+                                </a></li>
+                            <li><a href="https://www.instagram.com/Md_MusfikurRahman/" target="_blank">
+                                    <i class="fa fa-instagram"></i>
+                                </a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </main>
 
 
